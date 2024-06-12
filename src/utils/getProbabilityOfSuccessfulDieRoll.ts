5bc8dbc574b1a19df15bc7094a2da879efd2f00f
matveygrevtsov <matveygrevcov@gmail.com>
2024-06-12 07:54:43 +0300

    fix

--- src/utils/getProbabilityOfSuccessfulDieRoll.ts
+++ src/utils/getProbabilityOfSuccessfulDieRoll.ts
@@ -2,7 +2,8 @@ export const getProbabilityOfSuccessfulDieRoll = (
   minDieValue: number
 ): string => {
   const probability = ((7 - minDieValue) * 100) / 6;
+
   return `${
-    Number.isInteger(probability) ? probability : `~${probability.toFixed(2)}`
+    Number.isInteger(probability) ? probability : `~${Math.round(probability)}`
   }%`;
 };
